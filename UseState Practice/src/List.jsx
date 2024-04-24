bb2287c4512f125264d06beebc7f026cf85d8db2
krish736 <112505529+krish736@users.noreply.github.com>
2024-04-24 13:49:30 +0530

    commit

--- UseState Practice/src/List.jsx
+++ UseState Practice/src/List.jsx
@@ -1,13 +1,45 @@
+import { useState } from "react";
 import ListItems from "./ListItems";
 import styles from "./List.module.css";
 
 const List = () => {
-  // let items = ["Ghee", "Green Vegetables", "Fruits"];
-  let items = [];
+  let [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleOnKeyDown = (e) => {
+    if (e.key === "Enter") {
+      addItem();
+    }
+  };
+
+  const handleAddButtonClick = () => {
+    addItem();
+  };
+
+  const addItem = () => {
+    const inputElement = document.getElementById("itemInput");
+    const value = inputElement.value.trim();
+    if (value !== "") { 
+      let newItems = [...items, value];
+      setItems(newItems);
+      setInputValue(""); 
+    }
+  };
 
   return (
     <>
-      <input type="text" name="" id="" className={styles.in} />
+      <input
+        type="text"
+        name="itemInput"
+        id="itemInput"
+        className={styles.in}
+        onKeyDown={handleOnKeyDown}
+        value={inputValue} 
+        onChange={(e) => setInputValue(e.target.value)} 
+      />
+      <button className="addBtn" onClick={handleAddButtonClick}>
+        Add
+      </button>
       <ListItems items={items} />
     </>
   );
